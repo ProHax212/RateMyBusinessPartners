57bbd514c44656468a94bf4ac815c0025887f4a9
Ryan Comer <ryancomer94@gmail.com>
2016-02-03 16:23:40 -0600

    Created the skeleton for most of my part.  Added different activities and fragments to manage the reviews.  No logic yet, so the app should still run like before

---
+++ app/src/main/java/seniordesign/ratemybusinesspartners/models/Review.java
@@ -0,0 +1,11 @@
+package seniordesign.ratemybusinesspartners.models;
+
+/**
+ * Created by Ryan Comer on 2/3/2016.
+ */
+public class Review {
+
+    private String reviewText;
+    private int numStars;
+
+}
