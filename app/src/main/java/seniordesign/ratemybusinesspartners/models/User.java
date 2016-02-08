d25184faa5dccef7ab891c31f2035a87218affef
Abraham <abraham.chiu@utexas.edu>
2016-02-08 16:45:53 -0600

    2/8 User account login page and activity. Created Login, Register.java

--- app/src/main/java/seniordesign/ratemybusinesspartners/models/User.java
+++ app/src/main/java/seniordesign/ratemybusinesspartners/models/User.java
@@ -8,12 +8,14 @@ public class User {
     private String userName;
     private String email;
     private String password;
+    private String company;
 
 
-    public User(String userName, String password, String email){
+    public User(String userName, String password, String email, String company){
         this.userName = userName;
         this.password = password;
         this.email = email;
+        this.company = company;
     }
 
 
@@ -22,5 +24,34 @@ public class User {
     public String getUserName(){
         return this.userName;
     }
+
+    public String getPassword(){
+        return this.password;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+
+    public String getCompany(){
+        return this.company;
+    }
+
+    public void setUserName(String username){
+         this.userName = username;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public void setCompany(String company){
+        this.company = company;
+    }
+
 
 }
