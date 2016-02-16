57392da6e9e3cb45d90100fc4c3385d4de04c97f
Abraham <abraham.chiu@utexas.edu>
2016-02-16 12:58:58 -0600

    google login partially working. will proceed to deleting old user interface.

--- app/src/main/java/seniordesign/ratemybusinesspartners/Login.java
+++ app/src/main/java/seniordesign/ratemybusinesspartners/Login.java
@@ -4,37 +4,73 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+
+
 import org.w3c.dom.Text;
 
 import seniordesign.ratemybusinesspartners.models.User;
 
-public class Login extends AppCompatActivity implements View.OnClickListener {
-    String username, password;
-    Button login;
-    TextView register;
+public class Login extends AppCompatActivity implements  GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
+    EditText username, password;
+    private Button login;
+    private TextView register;
+    private TextView mStatusTextView;
+
+    private static final String TAG = "SignInActivity";
+    private GoogleApiClient mGoogleApiClient;
+    private static final int RC_SIGN_IN = 9001;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //EditText username, password;
-        Button login;
-        TextView register;
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        username = findViewById(R.id.login_editText_username).toString();
-        password = findViewById(R.id.login_editText_password).toString();
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
 
-        login = (Button) findViewById((R.id.login_button_login));
-        login.setOnClickListener(this);
+        // Build a GoogleApiClient with access to the Google Sign-In API and the
+        // options specified by gso.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        mStatusTextView = (TextView) findViewById(R.id.status);
+        username = (EditText) findViewById(R.id.login_editText_username);
+        password = (EditText) findViewById(R.id.login_editText_password);
+
+//        login = (Button) findViewById((R.id.sign_in_button));
+//        login.setOnClickListener(this);
+
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
+
+        SignInButton signInButton = (SignInButton) findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+        signInButton.setScopes(gso.getScopeArray());
 
         register = (TextView) findViewById((R.id.login_textView_register));
         register.setOnClickListener(this);
@@ -44,15 +80,66 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
     @Override
     public void onClick(View v) {
         switch(v.getId()) {
-            case R.id.login_button_login:
-                Intent login_intent = new Intent(this, CompanyProfile.class);
-                User user = new User(username, password);
-                login_intent.putExtra("newUser", user);
-                startActivity(login_intent);
+            case R.id.sign_in_button:
+//                Intent login_intent = new Intent(this, CompanyProfile.class);
+//                User user = new User(username, password);
+//                login_intent.putExtra("newUser", user);
+//                startActivity(login_intent);
+                signIn();
                 break;
             case R.id.login_textView_register:
                 startActivity(new Intent(this, Register.class));
                 break;
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d("request code", requestCode + "");
+        Log.d("result Code", resultCode + "");
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        // An unresolvable error has occurred and Google APIs (including Sign-In) will not
+        // be available.
+        Log.d(TAG, "onConnectionFailed:" + connectionResult);
+    }
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+            mStatusTextView.setText(getString(R.string.signed_in_fmt, acct.getDisplayName()));
+            Log.d("EMAIL IS", acct.getEmail());
+            Log.d("USERNAME IS", acct.getDisplayName());
+            updateUI(true);
+        } else {
+            // Signed out, show unauthenticated UI.
+            updateUI(false);
+        }
+    }
+
+    private void updateUI(boolean signedIn) {
+        if (signedIn) {
+            findViewById(R.id.sign_in_button).setVisibility(View.GONE);
+            findViewById(R.id.sign_out_and_disconnect).setVisibility(View.VISIBLE);
+        } else {
+            Log.d("LOGOUT", "LOGGED OUT");
+
+            findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
+            findViewById(R.id.sign_out_and_disconnect).setVisibility(View.GONE);
         }
     }
 }
