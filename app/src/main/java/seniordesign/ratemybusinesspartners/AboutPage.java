9d5e21030e530912cd152ef4f81795418e0f32f8
Abraham <abraham.chiu@utexas.edu>
2016-04-27 12:41:31 -0500

    open house version

--- app/src/main/java/seniordesign/ratemybusinesspartners/AboutPage.java
+++ app/src/main/java/seniordesign/ratemybusinesspartners/AboutPage.java
@@ -15,15 +15,6 @@ public class AboutPage extends AppCompatActivity {
         setContentView(R.layout.activity_about_page);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Emailing developers is not supported at this time.", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
